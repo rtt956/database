05bc50b4e35b221479c3e558508919982302f901
SuperChrim <70035611+SuperChrim@users.noreply.github.com>
2021-04-24 17:49:58 -0400

    Update Baseball.sql

--- Baseball.sql
+++ Baseball.sql
@@ -1,23 +1,35 @@
 create table team(
    team_id varchar(30) primary key,
+   team_name varchar(30),
+   wins int,
+   losses int
 );
 create table employee(
    emp_id varchar(30) primary key,
-   foreign key(team_id) references team(team_id)
+   foreign key(team_id) references team(team_id),
+   fname varchar(30),
+   lname varchar(30),
+   reports_to varchar(30)
 );
 create table contract(
    contract_id varchar(30) primary key,
-   foreign key(emp_id) references employee(emp_id)
+   foreign key(emp_id) references employee(emp_id),
+   date_start date,
+   date_finish date,
+   salary float
 );
 create table game(
    game_num varchar(30),
    foreign key(team_id) references team(team_id),
-   primary key(game_num,team_id)
+   primary key(game_num,team_id),
+   score int,
+   date date,
+   time int
 );
 create table park(
    park_id varchar(30) primary key,
    city varchar(30) not null,
-   state varchar(30) not null,
+   state varchar(30) not null
 );
 create table league(
    league_id varchar(30) primary key,
