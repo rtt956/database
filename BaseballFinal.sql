68288a5c7311f795bdf172107e3625e43f693905
rtt956 <78606652+rtt956@users.noreply.github.com>
2021-04-27 10:12:24 -0400

    Update BaseballFinal.sql

--- BaseballFinal.sql
+++ BaseballFinal.sql
@@ -5,6 +5,36 @@ create table team(
    wins int,
    losses int
 );
+
+#Team Data
+INSERT INTO `Team` (`team_id`,`team_name`,`wins`,`losses`) VALUES (1,"Tellus Inc.",5,9),
+(2,"Donec Nibh Quisque Incorporated",9,14),
+(3,"Malesuada Id Erat Corp.",3,22),
+(4,"Ut Semper Ltd",30,22),(5,"Habitant Morbi Institute",5,2),
+(6,"Id LLC",29,1),(7,"Eget Volutpat Ornare Consulting",26,2),
+(8,"Quis Accumsan Convallis Ltd",4,8),
+(9,"Consectetuer Adipiscing Elit Consulting",5,10),
+(10,"Ut Pharetra Limited",13,12),
+(11,"Vel Sapien Ltd",5,13),
+(12,"Conubia Nostra Per Corporation",27,1),
+(13,"Tincidunt Nunc Ac Associates",2,11),
+(14,"Enim Etiam Imperdiet Company",19,7),
+(15,"In Corporation",14,18),
+(16,"Massa LLP",24,18),
+(17,"Mauris Vel Turpis Consulting",15,10),
+(18,"Montes Nascetur Ridiculus Associates",8,16),
+(19,"Eu Dui Corporation",25,9),
+(20,"Et Magna Corporation",17,28),
+(21,"Velit Limited",1,7),
+(22,"Nec Quam LLC",8,17),
+(23,"Amet Risus Donec Incorporated",11,7),
+(24,"Erat Sed Inc.",27,11),
+(25,"Vitae Velit Corporation",24,13),
+(26,"Integer Ltd",27,18),
+(27,"Nec Tempus Mauris Industries",23,11),
+(28,"Mi Lorem Vehicula LLP",19,20),
+(29,"Cursus In LLC",7,29),
+(30,"Fames Ac Consulting",11,21);
 create table contract(
    contract_id varchar(30) primary key,
    date_start date,
